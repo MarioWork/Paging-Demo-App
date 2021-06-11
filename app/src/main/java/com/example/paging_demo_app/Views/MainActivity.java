bde17fdb65195903af167554841f0424474f8d25
Mario Vieria <47696178+MarioWork@users.noreply.github.com>
2021-06-11 00:51:28 +0000

    Updated MainActivity
    - Code Refactoring

--- app/src/main/java/com/example/paging_demo_app/Views/MainActivity.java
+++ app/src/main/java/com/example/paging_demo_app/Views/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
